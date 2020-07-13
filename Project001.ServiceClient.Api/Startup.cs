bbf5defd4dfaf5b721796c6a091acdb9273a1c1a
Bruno Aguiar Daguis <bruno.daguis@gmail.com>
2020-07-13 13:29:11 -0300

    Detalhando métodos para Swagger

--- Project001.ServiceClient.Api/Startup.cs
+++ Project001.ServiceClient.Api/Startup.cs
@@ -11,6 +11,10 @@ using Project001.ServiceClient.Infra.Repositories;
 using Project001.ServiceClient.Domain.Handlers.Interfaces;
 using Project001.ServiceClient.Domain.Handlers;
 using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using System.IO;
+using System;
+using Microsoft.OpenApi.Models;
 
 namespace Project001.ServiceClient.Api
 {
@@ -40,7 +44,27 @@ namespace Project001.ServiceClient.Api
             services.AddTransient<IClientRepository, ClientRepository>();
             services.AddTransient<IClientHandler, ClientHandler>();
 
-            services.AddSwaggerGen();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Service Client",
+                    Description = "A api with .net core 3.1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Bruno Daguis",
+                        Email = "bruno.daguis@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/bruno-daguis/"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +73,9 @@ namespace Project001.ServiceClient.Api
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projeto 001");
+                c.SwaggerEndpoint("/swagger/swagger/v1/swagger.json", "Projeto 001");
+                c.RoutePrefix = string.Empty;
+
             });
 
             app.UseRouting();
